6eba4d675b53fd2906ffe216eafc3bfca923c15b
Malaxiz <malaxiz.main@gmail.com>
2015-11-18 08:55:59 +0100

    Initial commit

---
+++ Languages/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Languages
+//
+//  Created by Didrik Munther on 18/11/15.
+//  Copyright © 2015 Didrik Munther. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
